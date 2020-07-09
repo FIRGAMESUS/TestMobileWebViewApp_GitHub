77e55c414b590f3de1541529e3e335f782db9cb7
tomashined <45771896+tomashined@users.noreply.github.com>
2020-07-09 17:47:27 +0300

    StartProject

---
+++ TestMobileWebViewApp/Assets/Scripts/MainManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Net;
+
+public class MainManager : MonoBehaviour
+{
+
+    void Start()
+    {
+        Debug.Log(IPManager.GetIP(ADDRESSFAM.IPv4));
+        var strHostName = "";
+        strHostName = System.Net.Dns.GetHostName();
+
+        var ipEntry = System.Net.Dns.GetHostEntry(strHostName);
+
+        var addr = ipEntry.AddressList;
+
+        var ip = addr[addr.Length - 1].ToString();
+        Debug.Log(ip);
+    }
+
+    
+    private void GoToGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    private void GoWeb()
+    {
+        SceneManager.LoadScene("WebViewScene");
+    }
+}
